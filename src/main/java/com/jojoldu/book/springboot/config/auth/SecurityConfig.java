4f6aa8634000e5dbdf6b876601b45a775a073d4e
faholoreach <faholoreach@naver.com>
2020-03-23 13:20:28 +0900

    Ch05-5.4: 어노테이션 기반으로 개선하기 완료.

---
+++ src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.jojoldu.book.springboot.config.auth;
+
+import com.jojoldu.book.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                    .anyRequest().authenticated()
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                        .defaultSuccessUrl("/", true) // info: google 로그인 후, 404 에러가 발생하여 추가한 메소드.
+                                                                                // > 어떤 문제가 있는지 좀 더 알아봐야 할 것으로 보인다.
+                        .userInfoEndpoint()
+                            .userService(customOAuth2UserService);
+    }
+}
