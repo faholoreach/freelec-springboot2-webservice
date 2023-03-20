9b8ab2063c94621cf843d09278021d71283aa88e
모노_정재혁 <faholoreach@monoalliance.com>
2023-03-20 18:12:54 +0900

    230320 p.211 Chapter 05.6

--- src/main/java/com/jojoldu/book/springboot/config/auth/SecurithConfig.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/SecurithConfig.java
@@ -14,14 +14,20 @@ public class SecurithConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
+                .csrf().disable()
                 .headers().frameOptions().disable()
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/css/**").permitAll()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                 .anyRequest().authenticated()
-                .and().logout().logoutSuccessUrl("/")
-                .and().oauth2Login().userInfoEndpoint().userService(customOAuth2UserService);
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
     }
 }
